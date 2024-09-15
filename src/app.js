dadeba78bfa19556232113ed7ee6fd52e31f465a
Jimena <jparatge@gmail.com>
2024-09-15 18:57:24 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["Mc Gyver", "Naruto", "Trump", "La madre Teresa"];
+  let action = ["bailó con", "desmayó", "saltó sobre", "rompió"];
+  let what = [
+    "el gato de la vecina",
+    "el ombligo de Shakira",
+    "los dientes de Suárez"
+  ];
+  let when = [
+    "en el mundial 2014.",
+    "cuando se crearon los mares.",
+    "durante mi siesta.",
+    "mientras corría el ómnibus.",
+    "después de robar un banco."
+  ];
+
+  function getRandom(array) {
+    let randomNumber = Math.floor(Math.random() * array.length);
+    return array[randomNumber];
+  }
+
+  let excusa = `
+${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)} 
+`;
+  console.log(excusa);
+  document.getElementById("excuse").innerHTML = excusa;
 };
